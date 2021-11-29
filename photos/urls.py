57560cf51a25ed816c6cbf594694369805b3e00b
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-30 00:44:41 +0300

    added urls.py for location/category

---
+++ photos/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url('^$',views.index,name='index'),
+    url('^today/$',views.today_photos,name='photosToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_photos,name = 'pastPhotos'),
+    url(r'^category/(\d+)',views.category,name='category'),
+    url(r'^location/(\d+)',views.location,name='location'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+

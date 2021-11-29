23d6c4737ca405d6942c2bfc2aa8495386168807
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-30 00:46:51 +0300

    creted class model for image

--- photos/models.py
+++ photos/models.py
@@ -34,3 +34,44 @@ class Location(models.Model):
     #     updated = cls.objects.filter(image_name=location).update(name=update)
     #     return updated
     
+class Image(models.Model):
+    image=CharField('image')
+    name=models.CharField(max_length =30)
+    description=models.TextField()
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    location=models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_photos(cls):
+
+        photos=cls.objects.all()
+        return photos
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image=cls.objects.get(id=image_id)
+        return image
+
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        photos=cls.objects.filter(category__name__icontains=search_term)
+        return photos
+
+    @classmethod
+    def filter_by_location(cls,location_id):
+        photos=cls.objects.filter(id=location_id)
+        return photos
+
+    @classmethod
+    def filter_by_category(cls,category_id):
+        photos=cls.objects.filter(id=category_id)
+        return photos

cbcbf5f5d6206e24fc65b39f41840d8f3a812f41
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-30 00:23:25 +0300

    created function to get past photos

--- photos/views.py
+++ photos/views.py
@@ -8,3 +8,23 @@ def index(request):
     photos=Image.get_photos()
 
     return render(request,'index.html',{"photos":photos})
+
+def today_photos(request):
+    date=dt.date.today()
+
+
+    return render(request,'all-photos/today-photos.html',{"date":date})
+
+
+def past_photos(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    if date == dt.date.today():
+        return redirect(today_photos)
+    return render(request,'all-photos/past-photos.html',{"date":date})

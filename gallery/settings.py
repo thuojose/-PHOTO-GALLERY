5b6c281d811a1d4fc43c0538d3b13ff0e853db11
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-30 01:00:01 +0300

    added database in settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'photo',
+           'USER': 'khalyz',
+           'PASSWORD': '12345'
+           
     }
 }
 

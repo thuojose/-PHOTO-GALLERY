464b80ef2026249ce58da3c66a8ea8d3b94e4c81
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-30 00:20:01 +0300

    created function for photos

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
+def index(request):
+
+    photos=Image.get_photos()
+
+    return render(request,'index.html',{"photos":photos})
